541de54bde02958035ece7c230bb01aced60faa7
thomaseade <eadethomas95@gmail.com>
2023-03-17 10:24:39 +0100

    arreglos

--- POO/contactsTest.js
+++ POO/contactsTest.js
@@ -8,7 +8,7 @@ let contacts =  new Contacts();
 
 let persona1 = new Person("Thomas", "Eade", 90, 180, 1995, ["padel", "pesca", "tenis", "cocinar"]);
 
-let persona2 = new Person ("Ana", "Perez", 70, 165, 1997, ["leer", "dormir", "TV", "pasear"])
+let persona2 = new Person("Ana", "Perez", 70, 165, 1997, ["leer", "dormir", "TV", "pasear"])
 
 
 contacts.persons.push(persona1);
